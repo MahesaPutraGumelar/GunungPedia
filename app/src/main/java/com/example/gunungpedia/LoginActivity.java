8f455e161410ec9437702b343720c83f6e8f317b
rifkyandry <rifkyandryanto7@gmail.com>
2024-11-29 11:02:03 +0700

    Menambahkan codingan logika di login

--- app/src/main/java/com/example/gunungpedia/LoginActivity.java
+++ app/src/main/java/com/example/gunungpedia/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.gunungpedia;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ImageView;
+import android.net.Uri;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +16,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email,pass;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+//        ImageView doodle = findViewById(R.id.google);
+//        ImageView twiter = findViewById(R.id.twiter);
+//        ImageView facbook = findViewById(R.id.facebook);
+
+//        google.setOnClikListener(v -> {
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            Intent.setData(Url.parse)
+//        })
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
